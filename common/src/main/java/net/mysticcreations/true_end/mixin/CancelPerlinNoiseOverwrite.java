49c530692268116c60976553321e17594e0e7302
eetgeenappels <eetgeenappels@gmail.com>
2025-10-15 18:29:19 +0200

    push the farlands fix when using C2Me

---
+++ common/src/main/java/net/mysticcreations/true_end/mixin/CancelPerlinNoiseOverwrite.java
@@ -0,0 +1,19 @@
+package net.mysticcreations.true_end.mixin;
+
+import com.bawnorton.mixinsquared.api.MixinCanceller;
+import java.util.List;
+
+public class CancelPerlinNoiseOverwrite implements MixinCanceller {
+
+
+    @Override
+    public boolean shouldCancel(List<String> list, String mixinClassName) {
+        if (mixinClassName.equals("com.ishland.c2me.opts.math.mixin.MixinOctavePerlinNoiseSampler") ||
+                mixinClassName.equals("com.ishland.c2me.opts.math.mixin.MixinPerlinNoiseSampler"))
+            return true;
+
+        return false;
+    }
+
+
+}
